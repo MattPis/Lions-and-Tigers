a5e7128af16011e4125006a006cfcef3d4fb4ffa
pisu86 <mateuszpiotrpis@gmail.com>
2015-01-16 21:53:18 -0600

    Initial Commit

---
+++ LionsAndTigers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LionsAndTigers
+//
+//  Created by Matt on 1/16/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
